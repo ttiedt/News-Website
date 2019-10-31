d961b0a8a0fb078acc7c916a0dd5740070517ea1
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-10-30 21:49:00 -0700

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,15 @@
 function article(){
   var title = "Second Coming Of Crypto";
-  var blurb = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-  return "<article><h3>"+ title + "</h3><p>" + blurb + "</p><div class='button'><a href='article.html'>View Article</a></div></article>";
+  var blurb = "The world's most-traded cryptocurrency has jumped 25% in a matter of days, rising above $9,300 in Monday trading from just under $7,500 on Oct. 25.";
+  return "<article><h3>"+ title + "</h3><p>" + blurb + "</p><div class='button center'><a href='article.html'>View Article</a></div></article>";
 }
-function addArticle(num, j){
-  for(var i = 0; i < num; i++)
+function addArticle(){
     document.getElementsByTagName('section')[j].insertAdjacentHTML('beforeend', article());
 }
 
+
 function bodyLoaded() {
   var category = document.getElementsByClassName("category");
-  var num = 1;
   for(var i = 0; i < category.length; i++){
     category[i].addEventListener("click", function(){
       this.classList.toggle("active");
@@ -23,9 +22,8 @@ function bodyLoaded() {
       else{ 
         content.style.display = "block";
         img.src = "images/down.png";
-        addArticle(3, i);
-        console.log(num);
-        num = num + 1;
+        content.getElementsByTagName('section')[0].innerHTML = '';
+        content.getElementsByTagName('section')[0].insertAdjacentHTML('beforeend', article().repeat(Math.floor(Math.random() * 12) + 4));
       }
     })
   };
